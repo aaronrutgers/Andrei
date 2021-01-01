f421d0f7a05fafe24ef6cec13b43296c429170b4
Can Li <>
2020-12-31 20:45:36 -0500

    finished

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function insertionSort(array) {
+function mergeSort (array) {
+  if (array.length === 1) {
+    return array
+  }
+  // Split Array in into right and left
   const length = array.length;
-	for (let i = 0; i < length; i++) {
-		if (array[i] < array[0]) {
-      //move number to the first position
-      array.unshift(array.splice(i,1)[0]);
-    } else {
-      // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
-      if (array[i] < array[i-1]) {
-        //find where number should go
-        for (var j = 1; j < i; j++) {
-          if (array[i] >= array[j-1] && array[i] < array[j]) {
-            //move number to the right spot
-            array.splice(j,0,array.splice(i,1)[0]);
-          }
-        }
-      }
-    }
-	}
+  const middle = Math.floor(length / 2)
+  const left = array.slice(0, middle) 
+  const right = array.slice(middle)
+  // console.log('left:', left);
+  // console.log('right:', right);
+
+  
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
 }
 
-insertionSort(numbers);
-console.log(numbers);
+function merge(left, right){
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && 
+        rightIndex < right.length){
+     if(left[leftIndex] < right[rightIndex]){
+       result.push(left[leftIndex]);
+       leftIndex++;
+     } else{
+       result.push(right[rightIndex]);
+       rightIndex++
+    }
+  }  
+  // console.log(left, right)
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+
+const answer = mergeSort(numbers);
+console.log(answer);
